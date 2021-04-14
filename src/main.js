fbaa2854b851b9f598944a86aebf62a0994ee695
Kendrick <kwlong@ucsc.edu>
2021-04-14 16:29:49 -0700

    Merge branch 'master' of https://github.com/SoloCircle/Rocket_Patrol

---
+++ src/main.js
@@ -0,0 +1,17 @@
+//game config
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play]
+}
+
+let game = new Phaser.Game(config);
+
+//set UI size
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+let starSpeed = 4;
+
+//reserve keybinds
+let keyF, keyR, keyLEFT, keyRIGHT;
