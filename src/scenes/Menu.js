32d0c7c9007b43c2a4b183ada751bd24e57aa227
Kendrick <kwlong@ucsc.edu>
2021-04-19 13:37:18 -0700

    fixes+art update

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,15 +9,14 @@ class Menu extends Phaser.Scene{
         this.load.audio('sfx_explosion', './assets/heal1.wav');
         this.load.audio('sfx_rocket', './assets/pop4.wav');
         this.load.audio('bgm_01', './assets/loginMusic.mp3');
-        this.load.audio('bgm_02', './assets/morning_breeze.mp3');
+        this.load.audio('bgm_02', './assets/morningMusic.mp3');
     }
 
     create(){
         let menuConfig = {
             fontFamily: 'Candara',
             fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
+            color: '#ffffff',
             align: 'right',
             padding: {
                 top: 5,
@@ -26,16 +25,14 @@ class Menu extends Phaser.Scene{
             fixedWidth: 0
         }
 
-        //play menusfx
 
 
         //menu text
-        this.add.text(150, 100, 'Star Catcher', menuConfig).setOrigin(0);
+        this.add.text(50, 50, 'Star Catcher', menuConfig).setOrigin(0);
         this.add.text(0, 360, 'Use <--> arrows to move & (F) to launch', menuConfig).setOrigin(0);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
+        //menuConfig.color = '#ffffff';
         this.add.text(0, 400,'Press <- for Dawn or -> for Dusk', menuConfig).setOrigin(0);
-        this.add.text(0, 440,'Press Escape During Game to Return to Menu', menuConfig).setOrigin(0);
+        this.add.text(0, 440,'Press (Esc) During Game to Return to Menu', menuConfig).setOrigin(0);
 
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
